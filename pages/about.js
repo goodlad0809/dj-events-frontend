7b1dffb348cab2dfc56e2987add8d05a7c0ca40d
goodlad0809 <UjBg9Beso%Ge>
2021-11-01 20:21:17 -0700

    first commit

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+export default function AboutPage() {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>this is a great app to find event information.</p>
+            <p>version: 1.0.0</p>
+        </div>
+    )
+}
